564de52c4d58175ef3b0df32647fa9d0fc3d08fa
ciph3rm0nk <ciph3rm0nk@gmail.com>
2021-06-21 16:15:25 -0600

    resolve issue with command being sent to system() failing because of return character

--- Wipe.pl
+++ Wipe.pl
@@ -13,10 +13,12 @@ use Term::Choose qw( choose );
 die "Must run as sudo or root\n" if $> != 0;
 
 # Need to have DCFLDD
-my $command = qx( which dcfldd ) || qx( which dx3dd );
+my $command = qx(which dcfldd) || qx(which dx3dd);
 if (!$command) {
 	die "This program needs dxfldd or dx3dd installed.\n You can install it via apt: \n\t \$ sudo apt install dcfldd|dc3dd \n";
-}
+} else { 
+	chomp( $command ); 
+	}
 
 # Get all of the disks by id (i think its easier to identify than by the classic numberation which can also be inconsistent)
 
@@ -50,15 +52,19 @@ if (scalar @disks > 0 ) {
 		}
 
 		my $dcfldd_disks = join(' ', @ofs);
+		chomp ($dcfldd_disks);
+
+		my @run0 = ($command, 'if=/dev/zero', $dcfldd_disks, 'conv=notrunc,noerror', 'bs=2M');
+		my @run1 = ($command, 'pattern=FF', $dcfldd_disks, 'conv=notrunc,noerror', 'bs=2M');
 
 		print "Running First Pass: Zeros to Disk";
-		system( qq( $command if=/dev/zero $dcfldd_disks conv=notrunc,noerror bs=2M ) );
+		system( @run0 );
 
 		print "Running Second Pass: Ones to Disk";
-		system( qq( $command pattern=FF $dcfldd_disks conv=notrunc,noerror bs=2M ) );
+		system( @run1 );
 		
 		print "Running Third Pass: Zeros to Disk";
-		system( qq( $command if=/dev/zero $dcfldd_disks conv=notrunc,noerror bs=2M ) );
+		system( @run0 );
 
 
 	} else {
